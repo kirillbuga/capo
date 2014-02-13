045936d1b6db6f718925e68abb21de866016dd18
Kirill Buga <tztimezero@yandex.ru>
2014-02-13 20:39:27 +0200

    Send search process to background. Added status messages on actions and results.

--- capo.py
+++ capo.py
@@ -1,9 +1,42 @@
-import sublime, sublime_plugin, re, os, functools
+import sublime, sublime_plugin, re, os, functools, threading
 
 def do_when(conditional, callback, *args, **kwargs):
 	if conditional():
 		return callback(*args, **kwargs)
 	sublime.set_timeout(functools.partial(do_when, conditional, callback, *args, **kwargs), 50)
+
+class SearchResult(object):
+	def __init__(self):
+		self.pathes = []
+		self.readable = []
+
+class SearchCall(threading.Thread):
+	def __init__(self, word_for_search, proj_folders):
+		self.text = word_for_search
+		self.proj_folders = proj_folders
+		self.result = None
+		self.nothing = False
+		threading.Thread.__init__(self)
+
+	def run(self):
+		result = SearchResult()
+		for dir in self.proj_folders:
+			files = None
+			for dir_path, subdir, files in os.walk(dir):
+				for file_name in files:
+					file_path = os.path.join(dir_path, file_name)
+					file = open(file_path, "r")
+					lines = file.readlines()					
+					for n, line in enumerate(lines):
+						if self.text in line:
+							result.pathes.append([file_path, n])
+							result.readable.append(file_name + ' @' + str(n + 1))
+					file.close()
+		if not len(result.pathes):
+			self.nothing = True
+
+		self.result = result;
+		return self.result
 
 class CapoCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -16,31 +49,32 @@ class CapoCommand(sublime_plugin.TextCommand):
 		content = view.substr(line) #return content of region
 		word = re.search('mediator.publish\\((\'|\")(.*)(\'|\")', content)
 		if word == None:
+			sublime.status_message('Can\'t find nothing in current line.')
 			return
 
 		#get folders to search
 		window = sublime.active_window()
 		proj_folders = window.folders()
+		word_for_search = str(word.group(2))
 
-		print "[Capo] Searching for " + word.group(2) + "..."
-		result = []
-		pathes = []
-		for dir in proj_folders:
-			files = None
-			for dir_path, subdir, files in os.walk(dir):
-				for file_name in files:
-					file_path = os.path.join(dir_path, file_name)
-					file = open(file_path, "r")
-					lines = file.readlines()
-					word_for_search = str(word.group(2))
-					for n, line in enumerate(lines):
-						if word_for_search in line:
-							result.append([file_path, n])
-							pathes.append(file_name + ' @' + str(n + 1))
-					file.close()
+		print "[Capo] Searching for " + word_for_search + "..."
 
-		window.show_quick_panel(pathes, lambda i: self.on_click(i, result))
-		print(result)
+		thread = SearchCall(word_for_search, proj_folders)
+		thread.start()
+		self.handle_threads(thread, window, view)
+
+	def handle_threads(self, thread, window, view):
+		if thread.is_alive():
+			view.set_status('Capo', 'Searching...')
+			sublime.set_timeout(lambda: self.handle_threads(thread, window, view), 100)
+			return
+		if thread.nothing == True:
+			sublime.status_message('No subscribers was found')
+			view.erase_status('Capo')
+			return
+
+		view.erase_status('Capo')
+		window.show_quick_panel(thread.result.readable, lambda i: self.on_click(i, thread.result.pathes))
 
 	#jump to file and set cursor to the given line
 	def jumpToFile(self, view, line):
